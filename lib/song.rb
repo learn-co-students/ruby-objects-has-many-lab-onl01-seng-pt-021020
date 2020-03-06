f288c926ba087394a1c2c3d5d7e1f3aac63c97fa
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-06 05:49:16 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Song
+  attr_accessor :artist, :name, :genre
+  @@all = []
+
+  def initialize(name)
+  @name = name
+  @genre = genre
+  @@all << self
+    end
+def self.all
+ @@all
+ end
+ def artist_name
+   if artist
+     self.artist.name
+   else
+     nil
+ end
+ end
+end
