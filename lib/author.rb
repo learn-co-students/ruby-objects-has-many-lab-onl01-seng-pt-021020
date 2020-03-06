b6453b367d18c5dec2950a993aa68065966ab12f
Andre Skeene <halodre24@gmail.com>
2020-03-06 22:47:41 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+# require "pry"
+class Author
+  attr_accessor :name
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def add_post(post)
+      @@post_count += 1
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+      @@post_count += 1
+      title = Post.new(title)
+      add_post(title)
+      # binding.pry
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
